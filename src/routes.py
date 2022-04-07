95edadcb2f480edd2cd7c4bfef4169528bf600c2
Jan Rundt <jan.rundt@gmail.com>
2022-04-07 16:40:34 +0300

    simpler generate

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,3 @@
-import os
 import random
 from flask import (
     render_template,
@@ -19,30 +18,21 @@ rsa_service = RsaService()
 @app.route('/')
 def base():
     file = random.randrange(100000,1000000)
-    html = f'<h1>Rsa-Service</h1><p>Please visit <a href="/createkeys/{file}">this page</a> to download your Rsa-keys.</p>'
+    html = f'<h1>Rsa-Service</h1><p>Please visit <a href="/genKeys">this page</a> to generate Rsa-keys.</p>'
     html += f'<p>Encrypt a message <a href="/encrypt">here</a></p>'
     html += f'<p>Decrypt a message <a href="/decrypt">here</a></p>'
     return html
 
-@app.route('/createkeys/<file>')
-def keys(file):
+@app.route('/genKeys')
+def gen_keys():
     (n,e,d) = rsa_key_gen.create()
-    f = open(os.path.join(app.root_path, f'temp/{file}.txt'), "w")
-    f.write(f'PUBLIC KEY:\n{n}#{e}\n\nPRIVATE KEY:\n{n}#{d}')
-    f.close()
-    html = f'<h1>Key generation</h1><p><b>Please download</b> your <a href="/download/{file}">keys</a></p>'
-    html += f'<p><b>After downloading</b>, please delete the keys from the server and <a href="/delete/{file}">return to the mainpage</a></p>'
+    html = '<h1>Your RSA-keys</h1><p>Please copy and store in a safe place</p>'
+    html += '<p>Return to <a href="/">the mainpage</a></p>'
+    html += '<h3>Public key</h3>'
+    html += f'<p style="overflow-wrap: break-word;">{n}#{e}</p>'
+    html += '<h3>Private key</h3>'
+    html += f'<p style="overflow-wrap: break-word;">{n}#{d}</p>'
     return html
-
-@app.route('/delete/<file>')
-def delete(file):
-    if os.path.exists(os.path.join(app.root_path, f'temp/{file}.txt')):
-        os.remove(os.path.join(app.root_path, f'temp/{file}.txt'))
-    return redirect('/')  
-
-@app.route('/download/<file>')
-def download(file):
-    return send_from_directory(os.path.join(app.root_path, 'temp'), f'{file}.txt', as_attachment=True)
 
 @app.route('/login')
 def login():
